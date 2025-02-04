183971da1d7acd8d8edc5cbdc4b5f6d56b3c67f7
hajarzain <hajarzain222@gmail.com>
2025-02-04 20:18:21 +0200

    Edited some files

--- src/pages/ResetPasswordPage/ResetPassword.jsx
+++ src/pages/ResetPasswordPage/ResetPassword.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../ResetPasswordPage/ResetPassword.css";
+import logo from "../../Images/Logo-Black.png";
+
 
 function ResetPassword () {
     const [password, setPassword] = useState("");
@@ -22,18 +24,28 @@ function ResetPassword () {
     return(
         <>
         <div className="header">
-            <img src="../assets/Logo ( Black ).png" alt="" />
+            <img src={ logo } alt="" />
             <button>Login</button>
         </div>
-        <div className="container">
-            <h2>Set a new password</h2>
-            <p>Your previous password has been reseted. Please set a new password for your account.</p>
-            <form onSubmit={handleReset} >
-                <input type="password" placeholder="New Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                <progress ></progress>
-                <input type="password" placeholder="Confirm New Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-                <button type="submit">Set Password</button>
-            </form>
+        <div className="reset-password">
+            <div className="reset-password__box">
+                <h2 className="reset-password__title">Set a new password</h2>
+                <p className="reset-password__subtitle">
+                    Your previous password has been reseted. Please set a new password for your account.
+                </p>
+                <form onSubmit={handleReset} className="reset-password__form">
+                    <div className="reset-password__input-container">
+                        <i class="fa-solid fa-lock reset-password__icon"></i>
+                        <input type="password" className="reset-password__input" placeholder="New Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    </div>
+                    <progress ></progress>
+                    <div className="reset-password__input-container">
+                        <i class="fa-solid fa-lock reset-password__icon"></i>
+                        <input type="password" className="reset-password__input" placeholder="Confirm New Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+                    </div>
+                    <button className="reset-password__button" type="submit">Set Password</button>
+                </form>
+            </div>
         </div>
         </>
     );
--- src/pages/VerifyCodePage/VerifyCode.jsx
+++ src/pages/VerifyCodePage/VerifyCode.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import "../VerifyCodePage/VerifyCode.css";
+import logo from "../../Images/Logo-Black.png";
+
 
 function VerifyCode () {
-
     const [code, setCode] = useState("");
 
     const handleVerify = (e) => {
@@ -16,19 +17,23 @@ function VerifyCode () {
     return(
         <>
         <div className="header">
-            <img src="../assets/Logo ( Black ).png" alt="" />
+            <img src={logo} alt="logo" />
             <button>Login</button>
         </div>
-        <div className="container">
-            <h2>Verify Code</h2>
-            <p>An authentication code has been sent to your email.</p>
-            <form onSubmit={handleVerify}>
-                <input className="" type="text" placeholder="Enter Code" value={code} onChange={(e) => setCode(e.target.value)} required />
-            </form>
-            <p>Didn&apos;t receive a code?
-                <a href="/reset-password"> Re-send</a>
-            </p>
-            <button type="submit">Submit</button>
+        <div className="verify-code">
+            <div className="verify-code__box">
+                <h2 className="verify-code__title">Verify Code</h2>
+                <p className="verify-code__subtitle">
+                    An authentication code has been sent to your email.
+                </p>
+                <form onSubmit={handleVerify} className="verify-code__form">
+                    <input className="verify-code__input" type="text" placeholder="Enter Code" value={code} onChange={(e) => setCode(e.target.value)} required />
+                </form>
+                <p className="verify-code__resend-text">Didn&apos;t receive a code?
+                    <a href="/reset-password" className="verify-code__resend-link"> Re-send</a>
+                </p>
+                <button className="verify-code__button" type="submit">Submit</button>
+            </div>
         </div>
         </>
     );
